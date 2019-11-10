c9fe3710b574971674401b64bcbdfb96bae53acf
musti <musti@wlan-si.net>
2019-11-10 21:32:15 +0100

    Implemented automatic setting json generation from .h

--- README.md
+++ README.md
@@ -1,89 +1,14 @@
-# smartparks-rgino-tracker-firmware
-Smartparks GPS tracker hardware repository, hereby we define features of the system and what needs to be built.
+# irnas-lorawan-base
+Universal code-base for multiple lorawan projects based on Arduino core. Modular structure allows the re-use of modules and features in multiple projects and thus the use of an universal code-base and regular updates.
 
-This code is being actively developed and is not yet suitable for use.
+### Downlink encoding
+To prepare data for the downlinks to happen, there is a json template 'encoded_settings_template.json' which is created using 'encoder_generator.py' each time fields the device sends change in the development process.
 
-# Requirements:
- * Modified Arduino Core for STM32L0
- * vscode or similar compile tool (Arduino IDE may break things)
- * this repository
- * TheThingsNetwork or other similar LoraWAN network server solution
-
-# Operation
-This device is a power-efficeint GPS tracker sending data via LoraWAN. There are three key messages being sent by the device:
- * `settings` - current device settings - only upon boot, change or request
- * `status` - device status message being sent with the specified `status_interval` in minutes - recommended to do this every 24h
-   * `system_function_erros` - indicate error of different sub-systems
-     * bit 0 - gps periodic error - sent when there is an issue comunicating with the GPS
-     * bit 1 - gps triggered error
-     * bit 2 - gps fix error - set if no fix has been acquired at all - note if 3D fix or target epe is not acquired we still consider this a valid fix
-     * bit 3 - accelerometer error
-     * bit 4 - light sensor error
-     * bit 5 - temperature error
-     * bit 6 - humidity sensor error
-     * bit 7 - pressure sensor error
- * `gps` - gps location - gps packet is set based on the `settings` and various intervals defined there.
-
-## Reed switch
-The magnetic REED switch is put in place to put the device in hibernation for transport purposes or prior to installation. Removing the magnet will initialize the operation in 60s of removing it. Note that putting the device in hibernation performs a system reset, including the provisioned settings. Provide settings via the network.
-
-## GPS acquisition logic
-The GPS system is configured by defining the `gps_periodic_interval` and other variables.
-
-
-## LoraWAN data
-See `decoder.js` which is rather human-readable for description of values being sent.
-
-## LoraWAN commands
-There are a number of single-byte commands specified for critical device control mechanisms. WARNING: Commands need to be sent unfonfirmed as they may reset the device, loosing the reply.
-
-Implemented commands to be sent as single byte to port 99:
- * 0xaa - Sent the current settings
- * 0xab - Triggers system reset
- * 0xde - Resets LoraWAN stored settings in EEPROM and forces a re-join
-
-## Tools
-There are a few tools available to make using this solution easier, namely:
- * `decoder.js` - TheThingsNetwork V2 payload decoder to jet nicely formatted json data from the binary messages
- * `encoder_settings_json.py` -Python scrip to prepare the json structure for settings to be sent to the device
- * `encoder.js` - TheThingsNetwork V2 payload encoder, accepting the above created json
-
-# Testing
-Various test to validate correct operation
-
-## Power consumption
-
-### System functions 0x00 - all disabled - sleep only with WDT running
-Measured consumption 2uA on average, excludes sending Lora messages.
-
-### System functions 0x01 - GPS periodic enabled
-Measured consumption 30uA on average, excludes sending Lora messages, expecting 15uA with GPS backup on
-
-## Function tests
-1. Reports GPS position in defined intervals - DONE
-1. Reports GPS position on motion activity (define send interval for this)
-1. GPS acquisition time is sub 10s at 1h interval
-1. Accepts new configuration via network - DONE
-1. Remote reset command can be sent - DONE
-1. OTAA parameters can be adjusted or reset - DONE
-1. Lora signal suitable and working well
-1. Survives gracefully the failure of all external componets (GPS, accelerometer, light) - DONE
-1. Correctly re-joins if link fails - NOT REQUIRED
-1. LoraWAN join fail backoff/retry period - NOT REQUIRED
-1. GPS cold-fix check -DONE
- * Tested GPS cold-fix timeout and hot-fix timeout - DONE
- * Evaluating when to switch to cold-fix - DONE
-1. GPS delay before shutdown check - DONE
-1. Does not polute the spectrum when voltage is low - DONE
-
-# TODO
-1. Implement settings for everything required
-1. Implement and test battery voltage measurement
-1. Evaluate power
-1. Clean-up
-
-
-
-# useful
-
-https://stackoverflow.com/questions/15638612/calculating-mean-and-standard-deviation-of-the-data-which-does-not-fit-in-memory
+The structure outline is as follows:
+ * module name
+   * setting name
+     * value - the value of the setting - to be modified by the user
+     * size - the variable size in bytes - to be used by the encoder
+     * order - order of the value in the payload - to be used by the encoder
+    
+The user can send the settings for each module individually or for multiple modules at the same time.
---
+++ encoder_generator.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+"""
+This code automatically fetches .h files which contain data structures representing binary data.
+A template json file is created with the deat structure representing bytes and values.
+User can then populate this tempalte with default values or otherwise and provide settings to the device.
+IMPORTANT: Tempalte must be rebuilt by running this script each time something changes.
+"""
+import sys
+import CppHeaderParser
+import json
+
+# This just makes the output more human readable
+def collapse_json(text, indent=12):
+    """Compacts a string of json data by collapsing whitespace after the
+    specified indent level
+
+    NOTE: will not produce correct results when indent level is not a multiple
+    of the json indent level
+    """
+    initial = " " * indent
+    out = []  # final json output
+    sublevel = []  # accumulation list for sublevel entries
+    pending = None  # holder for consecutive entries at exact indent level
+    for line in text.splitlines():
+        if line.startswith(initial):
+            if line[indent] == " ":
+                # found a line indented further than the indent level, so add
+                # it to the sublevel list
+                if pending:
+                    # the first item in the sublevel will be the pending item
+                    # that was the previous line in the json
+                    sublevel.append(pending)
+                    pending = None
+                item = line.strip()
+                sublevel.append(item)
+                if item.endswith(","):
+                    sublevel.append(" ")
+            elif sublevel:
+                # found a line at the exact indent level *and* we have sublevel
+                # items. This means the sublevel items have come to an end
+                sublevel.append(line.strip())
+                out.append("".join(sublevel))
+                sublevel = []
+            else:
+                # found a line at the exact indent level but no items indented
+                # further, so possibly start a new sub-level
+                if pending:
+                    # if there is already a pending item, it means that
+                    # consecutive entries in the json had the exact same
+                    # indentation and that last pending item was not the start
+                    # of a new sublevel.
+                    out.append(pending)
+                pending = line.rstrip()
+        else:
+            if pending:
+                # it's possible that an item will be pending but not added to
+                # the output yet, so make sure it's not forgotten.
+                out.append(pending)
+                pending = None
+            if sublevel:
+                out.append("".join(sublevel))
+            out.append(line)
+    return "\n".join(out)
+
+def fetch_settings_struct_from_header(filename,classname):
+    try:
+        cppHeader = CppHeaderParser.CppHeader(filename)
+    except CppHeaderParser.CppParseError as e:
+        #print(e)
+        print("File " + filename + " not found")
+    return cppHeader.classes[classname]["properties"]["public"]
+
+def create_array_from_settings(settings):
+    count = 0
+    length = 0
+    values={}
+    for item in settings:
+        values[item["name"]]={}
+        values[item["name"]]["value"]=0
+        length_bytes=validate_decode_variable_type(item["type"])
+        values[item["name"]]["size"]=length_bytes
+        length+=length_bytes
+        values[item["name"]]["order"]=count
+        count+=1
+    values["length"]["value"]=length
+    return values
+
+def validate_decode_variable_type(type):
+    #valid datatype lengths
+    data={"uint8_t":1,"uint16_t":2,"int8_t":1,"int16":2, "uint32_t":4, "uint64_t":8}
+    try:
+        return data[type]
+    except:
+        print("Incorrect data type, error! " + type)
+
+
+#create json template of all the settings
+
+template_settings={}
+
+### basic device settings
+settings = fetch_settings_struct_from_header("src/settings.h","settingsData_t")
+basic_settings=create_array_from_settings(settings)
+# add some data manually
+basic_settings["global_id"]["value"]=1
+#append to the template
+template_settings["basic_settings"]=basic_settings
+
+### module_system settings
+settings = fetch_settings_struct_from_header("src/module_system.h","MODULE_SYSTEM::module_settings_data_t")
+module_system=create_array_from_settings(settings)
+# add some data manually
+module_system["global_id"]["value"]=2
+#append to the template
+template_settings["module_system"]=module_system
+
+### module_gps_ublox settings
+settings = fetch_settings_struct_from_header("src/module_gps_ublox.h","MODULE_GPS_UBLOX::module_settings_data_t")
+module_gps_ublox=create_array_from_settings(settings)
+# add some data manually
+module_gps_ublox["global_id"]["value"]=3
+#append to the template
+template_settings["module_gps_ublox"]=module_gps_ublox
+
+### module_pira settings
+settings = fetch_settings_struct_from_header("src/module_pira.h","MODULE_PIRA::module_settings_data_t")
+module_pira=create_array_from_settings(settings)
+# add some data manually
+module_pira["global_id"]["value"]=4
+#append to the template
+template_settings["module_pira"]=module_pira
+
+### module_accelerometer settings
+settings = fetch_settings_struct_from_header("src/module_accelerometer.h","MODULE_ACCELEROMETER::module_settings_data_t")
+module_accelerometer=create_array_from_settings(settings)
+# add some data manually
+module_accelerometer["global_id"]["value"]=5
+#append to the template
+template_settings["module_accelerometer"]=module_accelerometer
+
+f = open('encoded_settings_template.json', 'w')
+f.write(collapse_json(json.dumps(template_settings, indent=4),indent=8))
+f.close()
+
+print(template_settings)
